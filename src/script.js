db6cb156f66fd3668103097c21238d7bc93bf99a
ishan <ishan.hcl.tab1@gmail.com>
2023-04-14 16:20:24 +0530

    gsap animation

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
+import './style.css'
 import * as THREE from 'three';
+import gsap from 'gsap';
+
+console.log(gsap);
 
 
 
@@ -7,33 +11,114 @@ const scene = new THREE.Scene()
 
 // Object
 
-const group = new THREE.Group();
-group.rotation.x = 1
-group.position.y = 1
-
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-group.add(cube1)
-cube1.position.x = -2
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
 
 
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-)
-group.add(cube2)
+
+// axesHelper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper);
 
 
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
-)
-cube3.position.x = 2
-group.add(cube3)
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600,
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// camera.position.x = 1
+// camera.position.y = 1
+camera.position.z = 3
+
+// look at the object , focus on object
+// camera.lookAt(mesh.position)
+
+// distance from camera
+// console.log(mesh.position.distanceTo(camera.position));
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas.webgl')
+})
+renderer.setSize(sizes.width, sizes.height)
+
+const clock = new THREE.Clock();
+
+gsap.to(mesh.position, {
+    x:2,
+    duration: 1,
+    delay: 1
+})
+gsap.to(mesh.position, {
+    x:-2,
+    duration: 1,
+    delay: 2
+})
+
+
+const tick = ()=> {
+    
+//     const elapsedTime = clock.getElapsedTime();
+//     // console.log(elapsedTime);
+
+//     mesh.position.y = Math.sin(elapsedTime) ;
+//     mesh.position.x = Math.cos(elapsedTime) ;
+
+//     // camera.position.y = Math.sin(elapsedTime) ;
+//     // camera.position.x = Math.cos(elapsedTime) ;
+//     // camera.lookAt(mesh.position)
+
+//     // one rotation per second
+//     // mesh.rotation.y = elapsedTime * Math.PI * 2 ;
+
+//     // render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick);
+
+}
+tick()
+
+
+
+
+
+
+// Object
+
+// const group = new THREE.Group();
+// group.rotation.x = 1
+// group.position.y = 1
+
+// scene.add(group);
+
+// const cube1 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1,1,1),
+//     new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// )
+// group.add(cube1)
+// cube1.position.x = -2
+
+
+// const cube2 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1,1,1),
+//     new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+// )
+// group.add(cube2)
+
+
+// const cube3 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1,1,1),
+//     new THREE.MeshBasicMaterial({ color: 0x0000ff })
+// )
+// cube3.position.x = 2
+// group.add(cube3)
 
 
 // const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -66,33 +151,19 @@ group.add(cube3)
 // mesh.position.normalize();
 // scene.add(mesh)
 
-// axesHelper
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper);
 
+// const tick = ()=> {
+//     const currentTime = Date.now();
+//     const deltaTime = currentTime - time;
+//     time = currentTime;
+//     console.log(deltaTime);
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600,
-}
+//     mesh.rotation.x += 0.001 * deltaTime
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// camera.position.x = 1
-// camera.position.y = 1
-camera.position.z = 3
+//     // render
+//     renderer.render(scene, camera)
 
-// look at the object , focus on object
-// camera.lookAt(mesh.position)
+//     window.requestAnimationFrame(tick);
 
-// distance from camera
-// console.log(mesh.position.distanceTo(camera.position));
-scene.add(camera)
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+// }
+// tick()
