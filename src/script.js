fbd12668d5d9ae3d19a555a789a4158afa2473ab
ishan <ishan.hcl.tab1@gmail.com>
2023-04-16 19:10:45 +0530

    full screen support

--- src/script.js
+++ src/script.js
@@ -1,12 +1,18 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import gsap from 'gsap';
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 const cursor = {
   x: 0,
   y: 0,
 };
+
 const canvas = document.querySelector("canvas.webgl");
 
 // cursor
@@ -26,14 +32,8 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // axesHelper
-// const axesHelper = new THREE.AxesHelper(2)
-// scene.add(axesHelper);
-
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600,
-};
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 const aspectRatio = sizes.width / sizes.height;
 
@@ -44,27 +44,20 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-// const camera = new THREE.OrthographicCamera(
-// -1 * aspectRatio,  // left
-//     1 * aspectRatio, // right
-//     1, // top
-//     -1, //bottom
-//     0.1,100)
-// camera.position.x = 2
-// camera.position.y = 2
+// camera.position.x = 2;
+// camera.position.y = 2;
 camera.position.z = 3;
 console.log(camera.position.length());
 
 // look at the object , focus on object
 camera.lookAt(mesh.position);
 
-// distance from camera
-// console.log(mesh.position.distanceTo(camera.position));
 scene.add(camera);
 
 // controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+// controls.enabled = false;
 // controls.target.y = 1;
 // controls.update();
 
@@ -78,22 +71,33 @@ renderer.setSize(sizes.width, sizes.height);
 // render
 renderer.render(scene, camera);
 
+// resizing of window
+window.addEventListener("resize", (event) => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  //   update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// full screen mode - using double click
+window.addEventListener("dblclick", () => {
+  console.log("double click");
+  if (!document.fullscreenElement) {
+    console.log("Go full screen mode");
+    canvas.requestFullscreen();
+  } else {
+    console.log("leave full screen mode");
+    document.exitFullscreen();
+  }
+});
+
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  // mesh.rotation.y = elapsedTime * Math.PI  ;
-  // console.log(cursor)
-
-  // render
-  // console.log(cursor)
-  //   camera.position.x = cursor.x * 10;
-  //   camera.position.y = cursor.y * 10;
-
-  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.y = cursor.y * 5;
-  //   camera.lookAt(mesh.position);
 
   //   update controls for damping
   controls.update();
@@ -103,6 +107,116 @@ const tick = () => {
   window.requestAnimationFrame(tick);
 };
 tick();
+
+//  -------------------------------------- camera , animation , controls ,  -------------
+
+// import "./style.css";
+// import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// // import gsap from 'gsap';
+
+// const cursor = {
+//   x: 0,
+//   y: 0,
+// };
+// const canvas = document.querySelector("canvas.webgl");
+
+// // cursor
+// window.addEventListener("mousemove", (event) => {
+//   cursor.x = event.clientX / sizes.width - 0.5;
+//   cursor.y = -(event.clientY / sizes.height - 0.5);
+// });
+
+// // Scene
+// const scene = new THREE.Scene();
+
+// // Object
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// // axesHelper
+// // const axesHelper = new THREE.AxesHelper(2)
+// // scene.add(axesHelper);
+
+// // Sizes
+// const sizes = {
+//   width: 800,
+//   height: 600,
+// };
+
+// const aspectRatio = sizes.width / sizes.height;
+
+// // Camera
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.1,
+//   100
+// );
+// // const camera = new THREE.OrthographicCamera(
+// // -1 * aspectRatio,  // left
+// //     1 * aspectRatio, // right
+// //     1, // top
+// //     -1, //bottom
+// //     0.1,100)
+// // camera.position.x = 2
+// // camera.position.y = 2
+// camera.position.z = 3;
+// console.log(camera.position.length());
+
+// // look at the object , focus on object
+// camera.lookAt(mesh.position);
+
+// // distance from camera
+// // console.log(mesh.position.distanceTo(camera.position));
+// scene.add(camera);
+
+// // controls
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+// // controls.target.y = 1;
+// // controls.update();
+
+// // Renderer
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: canvas,
+// });
+
+// renderer.setSize(sizes.width, sizes.height);
+
+// // render
+// renderer.render(scene, camera);
+
+// const clock = new THREE.Clock();
+
+// const tick = () => {
+//   const elapsedTime = clock.getElapsedTime();
+//   // mesh.rotation.y = elapsedTime * Math.PI  ;
+//   // console.log(cursor)
+
+//   // render
+//   // console.log(cursor)
+//   //   camera.position.x = cursor.x * 10;
+//   //   camera.position.y = cursor.y * 10;
+
+//   //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+//   //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+//   //   camera.position.y = cursor.y * 5;
+//   //   camera.lookAt(mesh.position);
+
+//   //   update controls for damping
+//   controls.update();
+
+//   renderer.render(scene, camera);
+
+//   window.requestAnimationFrame(tick);
+// };
+// tick();
+
+// ----------------------------- first time leanning  ------------------------------
 
 // Object
 
