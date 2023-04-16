e4e64e5af9e6854b9d5e50126f52d6d9a638a29c
ishan <ishan.hcl.tab1@gmail.com>
2023-04-16 16:41:49 +0530

    prettier and orbit controls and cursor orietation

--- src/script.js
+++ src/script.js
@@ -1,95 +1,108 @@
-import './style.css'
-import * as THREE from 'three';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import gsap from 'gsap';
 
 const cursor = {
-    x:0,
-    y:0,
-}
-window.addEventListener('mousemove', (event)=> {
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = -(event.clientY / sizes.height - 0.5);
-})
+  x: 0,
+  y: 0,
+};
+const canvas = document.querySelector("canvas.webgl");
+
+// cursor
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh)
-
-
+scene.add(mesh);
 
 // axesHelper
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper);
-
+// const axesHelper = new THREE.AxesHelper(2)
+// scene.add(axesHelper);
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600,
-}
+  width: 800,
+  height: 600,
+};
 
-const aspectRatio = sizes.width/sizes.height;
+const aspectRatio = sizes.width / sizes.height;
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100 )
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 // const camera = new THREE.OrthographicCamera(
-//     -1 * aspectRatio,
-//     1 * aspectRatio,
-//     1,
-//     -1,
+// -1 * aspectRatio,  // left
+//     1 * aspectRatio, // right
+//     1, // top
+//     -1, //bottom
 //     0.1,100)
 // camera.position.x = 2
 // camera.position.y = 2
-camera.position.z = 3
+camera.position.z = 3;
 console.log(camera.position.length());
 
 // look at the object , focus on object
-camera.lookAt(mesh.position)
+camera.lookAt(mesh.position);
 
 // distance from camera
 // console.log(mesh.position.distanceTo(camera.position));
-scene.add(camera)
+scene.add(camera);
 
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 1;
+// controls.update();
 
-const canvas = document.querySelector('canvas.webgl');
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
 
 // render
-renderer.render(scene, camera)
-
+renderer.render(scene, camera);
 
 const clock = new THREE.Clock();
 
-const tick = ()=> {
-    
-    const elapsedTime = clock.getElapsedTime();
-    // mesh.rotation.y = elapsedTime * Math.PI  ;
-    // console.log(cursor)
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  // mesh.rotation.y = elapsedTime * Math.PI  ;
+  // console.log(cursor)
 
-    // render
-    console.log(cursor.x)
-    camera.position.x = Math.sin(cursor.x * 2) ;
-    camera.position.Z = Math.cos(cursor.x * 2);
-    // camera.position.y = cursor.y * 5;
-    camera.lookAt(mesh.position)
+  // render
+  // console.log(cursor)
+  //   camera.position.x = cursor.x * 10;
+  //   camera.position.y = cursor.y * 10;
 
-    renderer.render(scene, camera)
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   camera.lookAt(mesh.position);
 
-    window.requestAnimationFrame(tick);
+  //   update controls for damping
+  controls.update();
 
-}
-tick()
+  renderer.render(scene, camera);
 
+  window.requestAnimationFrame(tick);
+};
+tick();
 
 // Object
 
@@ -106,13 +119,11 @@ tick()
 // group.add(cube1)
 // cube1.position.x = -2
 
-
 // const cube2 = new THREE.Mesh(
 //     new THREE.BoxGeometry(1,1,1),
 //     new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 // )
 // group.add(cube2)
-
 
 // const cube3 = new THREE.Mesh(
 //     new THREE.BoxGeometry(1,1,1),
@@ -120,7 +131,6 @@ tick()
 // )
 // cube3.position.x = 2
 // group.add(cube3)
-
 
 // const geometry = new THREE.BoxGeometry(1, 1, 1);
 // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
@@ -141,7 +151,7 @@ tick()
 // OR
 // mesh.scale.set(1,0.5,0.5)
 
-// Rotation 
+// Rotation
 // mesh.rotation.reorder('YXZ')
 // mesh.rotation.x = Math.PI * 0.25
 // mesh.rotation.y = Math.PI * 0.25
@@ -151,7 +161,6 @@ tick()
 // normalize the mesh, make distance to center 1, move to center
 // mesh.position.normalize();
 // scene.add(mesh)
-
 
 // const tick = ()=> {
 //     const currentTime = Date.now();
@@ -182,9 +191,8 @@ tick()
 //     delay: 2
 // })
 
-
 // const tick = ()=> {
-    
+
 // //     const elapsedTime = clock.getElapsedTime();
 // //     // console.log(elapsedTime);
 
