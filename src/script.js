c8ca8e13b7d9c0ae64f1c9467892f8750e490197
ishan <ishan.hcl.tab1@gmail.com>
2023-05-26 11:23:13 +0530

    particles

--- src/script.js
+++ src/script.js
@@ -15,222 +15,58 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-// fog
-const fog = new THREE.Fog("#262837", 1, 15);
-scene.fog = fog;
-
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-
-const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
-const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
-const doorAmbientOcclusionTexture = textureLoader.load(
-  "/textures/door/ambientOcclusion.jpg"
-);
-const doorheightTexture = textureLoader.load("/textures/door/height.jpg");
-const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
-const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
-const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
-
-const bricksColorTexture = textureLoader.load("/textures/bricks/color.jpg");
-const bricksNormalTexture = textureLoader.load("/textures/bricks/normal.jpg");
-const bricksRoughnessTexture = textureLoader.load(
-  "/textures/bricks/roughness.jpg"
-);
-const bricksAmbientOcclusionTexture = textureLoader.load(
-  "/textures/bricks/ambientOcclusion.jpg"
-);
-
-const grassColorTexture = textureLoader.load("/textures/grass/color.jpg");
-const grassNormalTexture = textureLoader.load("/textures/grass/normal.jpg");
-const grassRoughnessTexture = textureLoader.load(
-  "/textures/grass/roughness.jpg"
-);
-const grassAmbientOcclusionTexture = textureLoader.load(
-  "/textures/grass/ambientOcclusion.jpg"
-);
-
-grassColorTexture.repeat.set(8, 8);
-grassNormalTexture.repeat.set(8, 8);
-grassRoughnessTexture.repeat.set(8, 8);
-grassAmbientOcclusionTexture.repeat.set(8, 8);
-
-grassColorTexture.wrapS = THREE.RepeatWrapping;
-grassNormalTexture.wrapS = THREE.RepeatWrapping;
-grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
-grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
-
-grassColorTexture.wrapT = THREE.RepeatWrapping;
-grassNormalTexture.wrapT = THREE.RepeatWrapping;
-grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
-grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
-
-// Floor
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(20, 20),
-  new THREE.MeshStandardMaterial({
-    map: grassColorTexture,
-    normalMap: grassNormalTexture,
-    roughnessMap: grassRoughnessTexture,
-    aoMap: grassAmbientOcclusionTexture,
-  })
-);
-floor.geometry.setAttribute(
-  "uv2",
-  new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
-);
-floor.rotation.x = -Math.PI * 0.5;
-floor.position.y = 0;
-scene.add(floor);
+const particleTecture = textureLoader.load("/textures/particles/2.png");
 
 /**
- * House
+ * Test cube
+ */
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial()
+// );
+// scene.add(cube);
+
+/**
+ * particle
  */
 
-const house = new THREE.Group();
+// const particleGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particleGeometry = new THREE.BufferGeometry();
+const count = 5000;
+const position = new Float32Array(count * 3);
+const color = new Float32Array(count * 3);
 
-const walls = new THREE.Mesh(
-  new THREE.BoxGeometry(4, 2.5, 4),
-  new THREE.MeshStandardMaterial({
-    map: bricksColorTexture,
-    normalMap: bricksNormalTexture,
-    roughnessMap: bricksRoughnessTexture,
-    aoMap: bricksAmbientOcclusionTexture,
-  })
-);
-walls.geometry.setAttribute(
-  "uv2",
-  new THREE.Float32BufferAttribute(walls.geometry.attributes.uv.array, 2)
-);
-
-walls.position.y = 2.5 / 2;
-
-const roof = new THREE.Mesh(
-  new THREE.ConeGeometry(3.5, 1, 4),
-  new THREE.MeshStandardMaterial({ color: "#b35f45" })
-);
-roof.position.y = 0.5 + 2.5;
-roof.rotation.y = Math.PI / 4;
-
-const door = new THREE.Mesh(
-  new THREE.PlaneGeometry(2.2, 2.2, 100, 100),
-  new THREE.MeshStandardMaterial({
-    map: doorColorTexture,
-    alphaMap: doorAlphaTexture,
-    transparent: true,
-    aoMap: doorAmbientOcclusionTexture,
-    normalMap: doorNormalTexture,
-    roughnessMap: doorRoughnessTexture,
-    metalnessMap: doorMetalnessTexture,
-    displacementMap: doorheightTexture,
-    displacementScale: 0.1,
-  })
-);
-
-door.geometry.setAttribute(
-  "uv2",
-  new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
-);
-
-door.position.y = 1;
-door.position.z = 2.01;
-
-const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
-const bushMaterial = new THREE.MeshStandardMaterial({ color: "#89c854" });
-
-const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
-bush1.scale.set(0.5, 0.5, 0.5);
-bush1.position.set(0.8, 0.2, 2.2);
-
-const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
-bush2.scale.set(0.25, 0.25, 0.25);
-bush2.position.set(1.4, 0.1, 2.1);
-
-const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
-bush3.scale.set(0.4, 0.4, 0.4);
-bush3.position.set(-0.8, 0.1, 2.2);
-
-const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
-bush4.scale.set(0.15, 0.15, 0.15);
-bush4.position.set(-1, 0.05, 2.6);
-
-house.add(walls);
-house.add(roof);
-house.add(door);
-scene.add(bush1);
-scene.add(bush2);
-scene.add(bush3);
-scene.add(bush4);
-
-scene.add(house);
-
-// graves
-
-const graves = new THREE.Group();
-
-const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
-const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
-// const grave = new THREE.Mesh(graveGeometry, graveMaterial);
-
-// grave.position.y = 0.4;
-// grave.position.z = 3;
-// scene.add(grave);
-
-for (let i = 0; i < 50; i++) {
-  const isFallen = Math.random() < 0.1;
-  const angle = Math.random() * Math.PI * 2;
-  const radius = 3 + Math.random() * 6;
-  const x = Math.sin(angle) * radius;
-  const z = Math.cos(angle) * radius;
-
-  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
-  grave.castShadow = true;
-  grave.position.set(x, 0.3, z);
-  if (isFallen) {
-    grave.rotation.x = 1.6;
-    grave.position.y = 0.1;
-  } else {
-    grave.rotation.y = (Math.random() - 0.5) * 0.4;
-    grave.rotation.z = (Math.random() - 0.5) * 0.4;
-  }
-  scene.add(grave);
+for (let i = 0; i < count * 3; i++) {
+  position[i] = (Math.random() - 0.5) * 10;
+  color[i] = Math.random();
 }
 
-scene.add(graves);
+particleGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(position, 3)
+);
 
-/**
- * Lights
- */
-// Ambient light
-const ambientLight = new THREE.AmbientLight("#b9d5ff", 0.12);
-gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
-scene.add(ambientLight);
+particleGeometry.setAttribute("color", new THREE.BufferAttribute(color, 3));
 
-// Directional light
-const moonLight = new THREE.DirectionalLight("#b9d5ff", 0.12);
-moonLight.position.set(4, 5, -2);
-gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
-gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
-gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
-gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
-scene.add(moonLight);
+const particleMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+  //   color: "#ff88cc",
+  alphaMap: particleTecture,
+  transparent: true,
+  //   alphaTest: 0.001,
+  //   depthTest: false,
+  depthWrite: false,
+  blending: THREE.AdditiveBlending,
+  vertexColors: true,
+});
 
-const doorLight = new THREE.PointLight("#ff7d46", 1, 7);
-doorLight.position.set(0, 2.2, 2.7);
-scene.add(doorLight);
-
-//  ghost
-
-const ghost1 = new THREE.PointLight("#ff00ff", 2, 3);
-scene.add(ghost1);
-
-const ghost2 = new THREE.PointLight("#00ffff", 2, 3);
-scene.add(ghost2);
-
-const ghost3 = new THREE.PointLight("#ffff00", 2, 3);
-scene.add(ghost3);
+const particles = new THREE.Points(particleGeometry, particleMaterial);
+scene.add(particles);
 
 /**
  * Sizes
@@ -264,9 +100,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.x = 4;
-camera.position.y = 2;
-camera.position.z = 5;
+camera.position.z = 3;
 scene.add(camera);
 
 // Controls
@@ -279,42 +113,8 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
-renderer.setClearColor("#262837");
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-//  shadows
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-moonLight.castShadow = true;
-doorLight.castShadow = true;
-ghost2.castShadow = true;
-ghost1.castShadow = true;
-ghost3.castShadow = true;
-walls.castShadow = true;
-bush1.castShadow = true;
-bush2.castShadow = true;
-bush3.castShadow = true;
-bush3.castShadow = true;
-
-floor.receiveShadow = true;
-
-doorLight.shadow.mapSize.width = 256;
-doorLight.shadow.mapSize.height = 256;
-doorLight.shadow.mapSize.far = 7;
-
-ghost1.shadow.mapSize.width = 256;
-ghost1.shadow.mapSize.height = 256;
-ghost1.shadow.mapSize.far = 7;
-
-ghost2.shadow.mapSize.width = 256;
-ghost2.shadow.mapSize.height = 256;
-ghost2.shadow.mapSize.far = 7;
-
-ghost3.shadow.mapSize.width = 256;
-ghost3.shadow.mapSize.height = 256;
-ghost3.shadow.mapSize.far = 7;
 
 /**
  * Animate
@@ -324,22 +124,16 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  //   update ghosts
-  const ghost1Angle = elapsedTime * 0.5;
-  ghost1.position.x = Math.sin(ghost1Angle) * 4;
-  ghost1.position.z = Math.cos(ghost1Angle) * 4;
-  ghost1.position.y = Math.sin(elapsedTime * 3);
-
-  const ghost2Angle = -elapsedTime * 0.32;
-  ghost2.position.x = Math.sin(ghost2Angle) * 5;
-  ghost2.position.z = Math.cos(ghost2Angle) * 5;
-  ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
-
-  const ghost3Angle = -elapsedTime * 0.18;
-  ghost3.position.x =
-    Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
-  ghost3.position.z = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5));
-  ghost3.position.y = Math.sin(elapsedTime * 5) + Math.sin(elapsedTime * 2);
+  //   update particles
+  //   particles.rotation.x = elapsedTime * 0.02;
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    const x = particleGeometry.attributes.position.array[i3 + 0];
+    particleGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x * 1.5
+    );
+  }
+  particleGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();
