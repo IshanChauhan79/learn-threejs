93cc6d5c5c80fea74a1b5e5793bcc85db4a80104
ishan <ishan.hcl.tab1@gmail.com>
2023-08-06 18:43:11 +0530

    shaders patterns

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,27 +1,7 @@
-uniform mat4 projectionMatrix;
-uniform mat4 modelMatrix;
-uniform mat4 viewMatrix;
-
-attribute vec3 position;
-attribute float aRandom;
-attribute vec2 uv;
-
-
-uniform vec2 uFrequency;
-uniform float uTime;
-
-varying float vElevation;
 varying vec2 vUv;
 
-void main(){
-    vec4 modalPosition = modelMatrix * vec4(position, 1.0);
-    float elevation = sin(modalPosition.x * uFrequency.x - uTime) * 0.1 + sin(modalPosition.y * uFrequency.y - uTime) * 0.1;
-    modalPosition.z += elevation;
-
-    vec4 viewPosition = viewMatrix * modalPosition;
-    vec4 projectionPosition = projectionMatrix * viewPosition;
-    gl_Position = projectionPosition;
-
-    vUv = uv; 
-    vElevation= elevation;
+void main()
+{
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vUv = uv;
 }
